734b48388368366618db1a9b22c3e3f16f97ae5b
shashank <shashankvathar2718@gmail.com>
2026-09-03 11:51:29 +0530

    Configure Vercel serverless function to bundle server/dist output

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,4 @@
-import fs from 'fs';
-import path from 'path';
-
-let app: any;
-try {
-  const distPath = path.resolve(__dirname, '../server/dist/app.js');
-  if (fs.existsSync(distPath)) {
-    app = require('../server/dist/app').default || require('../server/dist/app');
-  } else {
-    app = require('../server/src/app').default || require('../server/src/app');
-  }
-} catch (e) {
-  app = require('../server/src/app').default || require('../server/src/app');
-}
+import app from '../server/dist/app';
 
 export default function handler(req: any, res: any) {
   return app(req, res);
